1743372968212a4b5b23fdde2c34eba02179caa0
AdvantagePupil <ahmadghaleb17@gmail.com>
2024-07-30 10:05:05 -0700

    first step

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
 	if (err)
 	{
-		return console.log(err);
+	return console.log(err);
 	}
-	process.stdout.write(data);
+process.stdout.write(data);
 });
