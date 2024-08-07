b66840d47919c6234ef71a2c78958957de993366
Ahmed Ghaleb <140106386+AdvantagePupil@users.noreply.github.com>
2024-08-08 00:37:36 +0300

    hot fix

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,3 +1,4 @@
-$('DIV#add_item').click(function () {
-  $('UL.my_list').append('<li>Item</li>');
-});
+$('div#add_item').click(function () {
+    let element = '<li>Item</li>';
+    $('ul.my_list').append(element);
+  });
